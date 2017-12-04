44e33a2c1073fd091099271f7715c103af9d0349
Mircea Staicu <md.staicu@yahoo.com>
2017-12-04 11:52:56 +0000

    Rollback webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,33 +1,27 @@
-var webpack = require('webpack');
+var webpack =  require('webpack');
 var path = require('path');
 
 const demoFileName = 'demo';
 
 module.exports = {
   entry: {
-    [demoFileName]: './demo/index.js'
+    [demoFileName]: './demo/index.js',
   },
   output: {
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: [path.resolve(__dirname, 'node_modules')],
-        enforce: 'pre',
-        use: 'eslint-loader'
+        use: 'babel-loader',
       },
-      {
-        test: /\.js$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
-        use: 'babel-loader'
-      }
-    ]
+    ],
   },
   devServer: {
     contentBase: path.join(__dirname, 'demo'),
     publicPath: '/dist/',
-    port: 1666
-  }
+    port: 1666,
+  },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,53 +3,34 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  bail: true,
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: [path.resolve(__dirname, 'node_modules')],
-        enforce: 'pre',
-        use: 'eslint-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
         use: 'babel-loader',
-      }
-    ]
+      },
+    ],
   },
-  plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        ie8: false,
-        ecma: 6,
-        output: {
-          comments: false,
-          beautify: false,
-        },
-      }
-    })
-  ],
+  plugins: [new webpack.optimize.ModuleConcatenationPlugin(), new UglifyJsPlugin()],
   externals: [
     {
       angular: 'angular',
       'tw-styleguide-components/dist/js/styleguide-components': {
         var: "''",
         commonjs: 'tw-styleguide-components/dist/js/styleguide-components',
-        commonjs2: 'tw-styleguide-components/dist/js/styleguide-components'
+        commonjs2: 'tw-styleguide-components/dist/js/styleguide-components',
       },
       'angular-cookies': {
         var: "'ngCookies'",
         commonjs: 'angular-cookies',
-        commonjs2: 'angular-cookies'
-      }
-    }
-  ]
+        commonjs2: 'angular-cookies',
+      },
+    },
+  ],
 };
