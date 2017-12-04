f2f900914625f18aabe604f31d520e41c3df628b
Mircea Staicu <md.staicu@yahoo.com>
2017-12-04 12:03:09 +0000

    Add .npmignore in order to preserve the dist folder when publishing to npm

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,34 +3,53 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  bail: true,
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: 'index.js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: [path.resolve(__dirname, 'node_modules')],
-        use: 'babel-loader',
+        enforce: 'pre',
+        use: 'eslint-loader',
       },
-    ],
+      {
+        test: /\.js$/,
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        use: 'babel-loader',
+      }
+    ]
   },
-  plugins: [new webpack.optimize.ModuleConcatenationPlugin(), new UglifyJsPlugin()],
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        ie8: false,
+        ecma: 6,
+        output: {
+          comments: false,
+          beautify: false,
+        },
+      }
+    })
+  ],
   externals: [
     {
       angular: 'angular',
       'tw-styleguide-components/dist/js/styleguide-components': {
         var: "''",
         commonjs: 'tw-styleguide-components/dist/js/styleguide-components',
-        commonjs2: 'tw-styleguide-components/dist/js/styleguide-components',
+        commonjs2: 'tw-styleguide-components/dist/js/styleguide-components'
       },
       'angular-cookies': {
         var: "'ngCookies'",
         commonjs: 'angular-cookies',
-        commonjs2: 'angular-cookies',
-      },
-    },
-  ],
+        commonjs2: 'angular-cookies'
+      }
+    }
+  ]
 };
